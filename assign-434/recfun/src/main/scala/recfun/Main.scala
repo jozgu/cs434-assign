4ff11e5484921dcc169109ff7aa88e3b0bff4e5d
s214617 <s214617@student.dtu.dk>
2023-09-12 11:41:17 +0900

    Implementation of pascal

---
+++ assign-434/recfun/src/main/scala/recfun/Main.scala
@@ -0,0 +1,38 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    // base case
+    if (c == 0 || c == r ) 1
+    // case of outside boundaries
+    else if (c <  0 || r < 0 || c > r) 0
+    // recursive case
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
+  }
+
+
+
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
+}
