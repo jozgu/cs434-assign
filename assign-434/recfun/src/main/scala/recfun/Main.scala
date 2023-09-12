3d68d57791593d8b8805bcd9715f4acd2ff7c108
s214617 <s214617@student.dtu.dk>
2023-09-12 12:18:10 +0900

    Implementation of countChange

--- assign-434/recfun/src/main/scala/recfun/Main.scala
+++ assign-434/recfun/src/main/scala/recfun/Main.scala
@@ -24,8 +24,6 @@ object Main {
   }
 
 
-
-
   /**
    * Exercise 2
    */
@@ -34,5 +32,16 @@ object Main {
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 1) {
+      1
+    } else if (money < 0 || coins.isEmpty) {
+      0
+    } else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+
+
+
+
+  }
+
 }
